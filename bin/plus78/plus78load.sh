2f6b0a60ab6cb4a533393a1d5b836a6532ee7901
sergey <malsn@rambler.ru>
2018-10-01 21:04:01 +0300

    controller path loadPlus78Action

--- bin/plus78/plus78load.sh
+++ bin/plus78/plus78load.sh
@@ -2,5 +2,5 @@
 /usr/bin/php /var/www/symfony-app/bin/plus78/plus78load.php
 f="/var/www/symfony-app/bin/plus78/SiteData.sql"
 if [[ -f $f ]]; then
-    mysql -u root -pTsIq4a0UwyE5ncFyQX -D myproject < $f
+    mysql -u user_mp -pTsIq4a0UwyE5ncFyQX -D myproject < $f
 fi
